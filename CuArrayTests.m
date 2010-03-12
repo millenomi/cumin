8a14cf9d8e7d85af1b5fddcdd7255a23ebf8d558
Emanuele Vulcano (Infinite Labs) <me@infinite-labs.net>
2010-03-12 19:40:10 +0100

    Extended tests on mutable arrays to include insertion after removal, head insertion, tail insertion.

--- CuArrayTests.m
+++ CuArrayTests.m
@@ -20,26 +20,44 @@
 	CuArrayAddObject(m, CuNumberMakeFromInteger(34));
 	CuArrayAddObject(m, CuNumberMakeFromInteger(567));
 	
-	STAssertTrue(CuArrayGetCount(m) == 3, @"Three items in the array");
+	STAssertTrue(CuArrayGetCount(m) == 3, @"Right count for items in the array");
 	
 	CuArrayInsertObjectAtIndex(m, CuNumberMakeFromInteger(888), 1);
-	STAssertTrue(CuArrayGetCount(m) == 4, @"Four items in the array");
+	STAssertTrue(CuArrayGetCount(m) == 4, @"Right count for items in the array");
 	STAssertTrue(CuNumberGetInteger(CuArrayGetObjectAtIndex(m, 0)) == 12, @"The right item was returned");
 	STAssertTrue(CuNumberGetInteger(CuArrayGetObjectAtIndex(m, 1)) == 888, @"The right item was returned");
 	STAssertTrue(CuNumberGetInteger(CuArrayGetObjectAtIndex(m, 2)) == 34, @"The right item was returned");
 	STAssertTrue(CuNumberGetInteger(CuArrayGetObjectAtIndex(m, 3)) == 567, @"The right item was returned");
 
 	CuArrayRemoveObjectAtIndex(m, 2);
-	STAssertTrue(CuArrayGetCount(m) == 3, @"Three items in the array");
+	STAssertTrue(CuArrayGetCount(m) == 3, @"Right count for items in the array");
 	STAssertTrue(CuNumberGetInteger(CuArrayGetObjectAtIndex(m, 0)) == 12, @"The right item was returned");
 	STAssertTrue(CuNumberGetInteger(CuArrayGetObjectAtIndex(m, 1)) == 888, @"The right item was returned");
 	STAssertTrue(CuNumberGetInteger(CuArrayGetObjectAtIndex(m, 2)) == 567, @"The right item was returned");
 	
 	CuArray* im = CuArrayMakeCopy(m);
-	STAssertTrue(CuArrayGetCount(im) == 3, @"Three items in the array");
+	STAssertTrue(CuArrayGetCount(im) == 3, @"Right count for items in the array");
 	STAssertTrue(CuNumberGetInteger(CuArrayGetObjectAtIndex(im, 0)) == 12, @"The right item was returned");
 	STAssertTrue(CuNumberGetInteger(CuArrayGetObjectAtIndex(im, 1)) == 888, @"The right item was returned");
-	STAssertTrue(CuNumberGetInteger(CuArrayGetObjectAtIndex(im, 2)) == 567, @"The right item was returned");	
+	STAssertTrue(CuNumberGetInteger(CuArrayGetObjectAtIndex(im, 2)) == 567, @"The right item was returned");
+	
+	CuArrayRemoveObjectAtIndex(m, 0);
+	STAssertTrue(CuArrayGetCount(m) == 2, @"Right count for items in the array");
+	STAssertTrue(CuNumberGetInteger(CuArrayGetObjectAtIndex(m, 0)) == 888, @"The right item was returned");
+	STAssertTrue(CuNumberGetInteger(CuArrayGetObjectAtIndex(m, 1)) == 567, @"The right item was returned");	
+
+	CuArrayAddObject(m, CuNumberMakeFromInteger(909));
+	STAssertTrue(CuArrayGetCount(m) == 3, @"Right count for items in the array");
+	STAssertTrue(CuNumberGetInteger(CuArrayGetObjectAtIndex(m, 0)) == 888, @"The right item was returned");
+	STAssertTrue(CuNumberGetInteger(CuArrayGetObjectAtIndex(m, 1)) == 567, @"The right item was returned");	
+	STAssertTrue(CuNumberGetInteger(CuArrayGetObjectAtIndex(m, 2)) == 909, @"The right item was returned");
+
+	CuArrayInsertObjectAtIndex(m, CuNumberMakeFromInteger(1010), 0);
+	STAssertTrue(CuArrayGetCount(m) == 4, @"Right count for items in the array");
+	STAssertTrue(CuNumberGetInteger(CuArrayGetObjectAtIndex(m, 0)) == 1010, @"The right item was returned");
+	STAssertTrue(CuNumberGetInteger(CuArrayGetObjectAtIndex(m, 1)) == 888, @"The right item was returned");
+	STAssertTrue(CuNumberGetInteger(CuArrayGetObjectAtIndex(m, 2)) == 567, @"The right item was returned");	
+	STAssertTrue(CuNumberGetInteger(CuArrayGetObjectAtIndex(m, 3)) == 909, @"The right item was returned");
 }
 
 @end
